6c88ba71614c583654d2b49f803a8f22dfcdb8d7
Dan <danyost23@gmail.com>
2024-06-11 10:03:21 -0400

    Refactor for pug, separate pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,51 @@
 const path = require('path');
+const Pug = require('pug-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/assets/main.js',
+  plugins: [
+    new Pug({
+      pretty: 'auto', // format HTML in dev mode
+      entry: {
+        index: './src/index.pug',
+        crafts: './src/crafts.pug',
+        about: './src/about.pug',
+      },
+      js: {
+        filename: './assets/[name].js'
+      },
+      css: {
+        filename: './assets/[name].css'
+      },
+    }),
+  ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "./assets/[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '',
+    assetModuleFilename: (pathData) => {
+      const filepath = path
+        .dirname(pathData.filename)
+        .split("/")
+        .slice(1)
+        .join("/");
+      return `${filepath}/[name][ext][query]`;
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        loader: 'css-loader',
+        options: {
+          import: true,
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
