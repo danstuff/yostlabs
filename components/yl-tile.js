301ef63c8536c8464fe104e94a5f61ef8ecf7ad4
danstuff <danyost23@gmail.com>
2025-06-29 00:40:45 -0400

    Tile styling and structure

--- components/yl-tile.js
+++ components/yl-tile.js
@@ -3,17 +3,33 @@ import ylComponent from "./yl-component";
 export default class ylTile extends ylComponent {
 
   static get observedAttributes() {
-    return [];
+    return ['title', 'image'];
   }
 
   get html() {
     return `
-      <p>${this.title}</p>
+      <a href="#">
+        <img part="image" src="${this.image}"></img>
+        <p part="title">${this.title}</p>
+      </a>
     `;
   }
 
   get css() {
-    return ``;
+    return `
+      :host {
+        display: inline-block;
+        width: fit-content;
+      }
+
+      a {
+        display: inline-flex;
+        flex-direction: column;
+        align-items:center;
+        text-decoration: inherit;
+        color: inherit;
+      }
+    `;
   }
 }
 
