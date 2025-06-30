92325ee5bfbd26f82ff1a899de14d0a8a57211d1
danstuff <danyost23@gmail.com>
2025-06-30 19:21:03 -0400

    Fix other modal style bug

--- components/yl-modal.js
+++ components/yl-modal.js
@@ -36,10 +36,14 @@ export default class ylModal extends ylComponent {
         position: fixed;
         width: 100%;
         height: 100%;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
       }
 
       :host([open]) {
-        display: flex;
+        display: block;
         z-index: 200;
       }
       
