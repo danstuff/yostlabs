0e6ebb750a5d705896528df69af799b9300322cd
danstuff <danyost23@gmail.com>
2025-06-30 22:17:19 -0400

    Add yl-if

---
+++ components/yl-if.js
@@ -0,0 +1,22 @@
+import ylComponent from "./yl-component";
+
+export default class ylIf extends ylComponent {
+  static get observedAttributes() {
+    return ['truthy', 'falsy'];
+  }
+
+  get html() {
+    const attrTruthy = (attribute) => {
+      return this.hasAttribute(attribute) &&
+             this.getAttribute(attribute) != "";
+    }
+
+    if (attrTruthy('truthy') || !attrTruthy('falsy')) {
+      return `<slot></slot>`;
+    } else {
+      return ``;
+    }
+  }
+}
+
+ylIf.defineElement();
