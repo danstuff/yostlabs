cbc44af623ffecadcd9d025cec3864df5c889933
danstuff <danyost23@gmail.com>
2025-07-01 01:23:28 -0400

    Remove background from modal

--- components/yl-modal.js
+++ components/yl-modal.js
@@ -8,24 +8,21 @@ export default class ylModal extends ylComponent {
 
   get html() {
     return `
-      <div part="background"></div>
-      <div part="container">
-        <a part="closeButton" href="#">
-          <svg
-            width="100%"
-            height="100%"
-            viewBox="0 0 100 100">
-            <path
-                style="stroke:white;stroke-width:0.75em;"
-                d="M 5,95 95,5"/>
-            <path
-                style="stroke:white;stroke-width:0.75em;"
-                d="M 5,5 95,95"/>
-          </svg>
-        </a>
-        <div part="content">
-          <slot></slot>
-        </div>
+      <a part="closeButton" href="#">
+        <svg
+          width="100%"
+          height="100%"
+          viewBox="0 0 100 100">
+          <path
+              style="stroke:white;stroke-width:0.75em;"
+              d="M 5,95 95,5"/>
+          <path
+              style="stroke:white;stroke-width:0.75em;"
+              d="M 5,5 95,95"/>
+        </svg>
+      </a>
+      <div part="content">
+        <slot></slot>
       </div>
     `;
   }
@@ -33,42 +30,27 @@ export default class ylModal extends ylComponent {
   get css() {
     return `
       :host {
-        display: none;
+        visibility: hidden;
         position: fixed;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        bottom: 0;
+        margin: auto;
         left: 0;
         right: 0;
-      }
-
-      :host([open]) {
-        display: block;
-        z-index: 200;
-      }
-      
-      div[part="container"] {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+        top: 0;
+        bottom: 0;
         max-width: 95%;
         max-height: 95%;
         z-index: 200;
-        overflow-y: auto;
+        background-color: inherit;
+      }
+
+      :host([open]) {
+        visibility: visible;
       }
 
       div[part="content"] {
         display: flex;
         flex-direction: column;
-        height: 100%;
-      }
-
-      div[part="background"] {
-        position: fixed;
-        z-index: 150;
-        width: 100%;
+        overflow-y: auto;
         height: 100%;
       }
 
@@ -82,6 +64,10 @@ export default class ylModal extends ylComponent {
 
       a[part="closeButton"] svg {
         pointer-events: none;
+      }
+        
+      :host([open]) a {
+        display: block;
       }
     `;
   }
@@ -100,7 +86,9 @@ export default class ylModal extends ylComponent {
   onClick(e) {
     if (e.target == this.dom.closeButton ||
         e.target == this.dom.background) {
-      this.open = false;
+      setTimeout(() => {
+        this.open = false;
+      }, 100);
     }
   }
 
