f6eb1439178b833f9899f2ecf8c6591af815421d
danstuff <danyost23@gmail.com>
2025-07-07 21:44:57 -0400

    Add aspect attribute to imageview

--- components/yl-image-view.js
+++ components/yl-image-view.js
@@ -3,13 +3,13 @@ import ylComponent from "./yl-component";
 export default class ylImageView extends ylComponent {
 
   static get observedAttributes() {
-    return ['src', 'vignette'];
+    return ['src', 'aspect', 'vignette'];
   }
 
   get html() {
     return `
-    <svg width="100%" height="100%" preserveAspectRatio="xMidYMid slice" focusable="true">
-      <image width="100%" height="100%" preserveAspectRatio="xMidYMid slice" href="${this.src}" />
+    <svg width="100%" height="100%" focusable="true">
+      <image width="100%" height="100%" preserveAspectRatio="${this.aspect || "xMidYMid slice"}" href="${this.src}" />
       ${ this.vignette ? `
         <radialGradient fx="50%" fy="50%" r="75%" spreadMethod="pad" id="grad{{index}}">
           <stop offset="0%" stop-color="#000000" stop-opacity="0.0" />
