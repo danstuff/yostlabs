efd65d8dea7f1d08a15e40902c3ebf1265501646
danstuff <danyost23@gmail.com>
2025-07-08 23:16:22 -0400

    Modal resizing

--- components/yl-modal.js
+++ components/yl-modal.js
@@ -8,13 +8,18 @@ export default class ylModal extends ylComponent {
 
   get html() {
     return `
-      <div part="actions">
-        <a part="maximize" href="#">&#9632;</a>
-        <a part="close" href="#">x</a>
+      <div part="titlebar" draggable="true">
+        <slot name="title"></slot>
+        <div class="spacer"></div>
+        <div part="actions">
+          <button part="maximize" href="#">&#9633;</button>
+          <button part="close" href="#">X</button>
+        </div>
       </div>
       <div part="content">
         <slot></slot>
       </div>
+      <button part="resize" draggable="true">&#9698;</button>
     `;
   }
 
@@ -26,12 +31,15 @@ export default class ylModal extends ylComponent {
         margin: auto;
         left: ${this.x || 0}px;
         top: ${this.y || 0}px;
-        width: ${this.width || 1000}px;
-        height: ${this.height || 800}px;
+        width: ${this.width || 640}px;
+        height: ${this.height || 480}px;
         max-width: 95%;
         max-height: 95%;
         z-index: 200;
         background-color: inherit;
+        font-family: inherit;
+        display: flex;
+        flex-direction: column;
       }
 
       :host([opened]) {
@@ -46,30 +54,46 @@ export default class ylModal extends ylComponent {
         max-width: 100%;
         max-height: 100%;
       }
-      
+
+      div[part="titlebar"] {
+        width: 100%;
+        display: flex;
+      }
+
       div[part="content"] {
-        display: flex;
-        flex-direction: column;
-        height: 100%;
+        flex-grow: 1;
       }
 
-      div[part="actions"] {
-        position: absolute;
-        top: 0;
-        right: 0;
-        display: flex;
+      div.spacer {
+        flex-grow: 1;
+        cursor: grab;
       }
 
-      a {
+      button {
         text-decoration: none;
         color: inherit;
+        background: none;
+        border: none;
+        padding: 0;
+        font: inherit;
+        font-family: inherit;
+        cursor: pointer;
+      }
+
+      button[part="resize"] {
+        position: absolute;
+        bottom: 1px;
+        right: 2px;
+        cursor: grab;
       }
     `;
   }
 
   mapDOM() {
-    this.dom.close = this.shadowRoot.querySelector('a[part="close"]');
-    this.dom.maximize = this.shadowRoot.querySelector('a[part="maximize"]');
+    this.dom.close = this.shadowRoot.querySelector('button[part="close"]');
+    this.dom.maximize = this.shadowRoot.querySelector('button[part="maximize"]');
+    this.dom.resize = this.shadowRoot.querySelector('button[part="resize"]');
+    this.dom.titlebar = this.shadowRoot.querySelector('div[part="titlebar"]')
     this.dom.template = this.querySelector('template') || {};
 
     this.dom.close.onclick = () => {
@@ -80,17 +104,43 @@ export default class ylModal extends ylComponent {
       this.maximized = !this.maximized;
     }
 
-    this.ondragstart = (e) => {
+    this.dom.titlebar.ondragstart = (e) => {
       this.dragX = e.clientX;
       this.dragY = e.clientY;
     }
 
-    this.ondragend = (e) => {
+    this.dom.titlebar.ondragend = (e) => {
+      if (this.maximized) {
+        return;
+      }
+
       const dx = this.dragX - e.clientX;
       const dy = this.dragY - e.clientY;
 
       this.x = (this.offsetLeft - dx);
       this.y = (this.offsetTop - dy);
+    }
+
+    this.dom.resize.ondragstart = (e) => {
+      this.dragX = e.clientX;
+      this.dragY = e.clientY;
+    }
+
+    this.dom.resize.ondragend = (e) => {
+      if (this.maximized) {
+        return;
+      }
+
+      const dx = this.dragX - e.clientX;
+      const dy = this.dragY - e.clientY;
+
+      this.width = (this.offsetWidth - dx);
+      this.height = (this.offsetHeight - dy);
+      
+      this.width = Math.max(this.width, 240);
+      this.height = Math.max(this.height, 240);
+
+      e.preventDefault();
     }
 
     this.bind(document,
@@ -101,8 +151,6 @@ export default class ylModal extends ylComponent {
 
   open(e) {
     this.opened = true;
-    this.x = 0;
-    this.y = 0;
     this.fillStubs(this.dom.template, e.target);
   }
 }
